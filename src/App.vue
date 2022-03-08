103818dfd6854b10e88de0891672b0232f7e1b4b
Ryan Harmon <heyharmon@gmail.com>
2022-03-07 17:01:17 -0700

    Add fields to table block

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="blocks">
     <!-- <Navbar :primaryLinks="primaryLinks" /> -->
     <!-- <Navbar :menu="menu"/> -->
     
@@ -8,46 +8,43 @@
     <!-- <Accordion/> -->
     <!-- <CardRepeater/> -->
     <!-- <DetailsList/> -->
-    <Feature/>
+    <!-- <Feature flip="horizontal"/> -->
+    <!-- <Feature flip="horizontal"/> -->
     <!-- <FeatureV7/> -->
     <!-- <FeatureV9/> -->
     <!-- <FeatureV11/> -->
     <!-- <BoxedHero/> -->
-    <Hero :center="false" :fullscreen="false"/>
+    <!-- <Hero :config="{ headingLevel: '3', headingSize: 'xl' }"/> -->
     <!-- <VideoBackgroundHero/> -->
     <!-- <Modal uuid="123" title="The modal title"/> -->
     <!-- <SubNavigation/> -->
-    <AppTable/>
+    <!-- <AppTable/> -->
     <!-- <Testimonial/> -->
     <!-- <TextColumns :cols="'2'"/> -->
+    
+    <component
+      v-for="(block, index) in blocks"
+      v-bind="block.data"
+      :is="block.component"
+      :key="index"
+    />
+    
+    <pre>{{ blocks[0] }}</pre>
   </div>
 </template>
 
-<script setup lang="ts">
-// Globals
-// import Navbar from './lib-components/globals/Navbar/Navbar.vue'
-import Navbar from './lib-components/globals/Header/Navbar.vue'
+<script setup>
+import { ref, onMounted } from '@vue/composition-api'
 
-// Blocks
-import Accordion from './lib-components/blocks/Accordion/Accordion.vue'
-import CardRepeater from './lib-components/blocks/CardRepeater/CardRepeater.vue'
-import DetailsList from './lib-components/blocks/DetailsList/DetailsList.vue'
-import Feature from './lib-components/blocks/Feature/Feature.vue'
-import FeatureV7 from './lib-components/blocks/Feature/FeatureV7.vue'
-import FeatureV9 from './lib-components/blocks/Feature/FeatureV9.vue'
-import FeatureV11 from './lib-components/blocks/Feature/FeatureV11.vue'
-import BoxedHero from './lib-components/blocks/Hero/BoxedHero.vue'
-import Hero from './lib-components/blocks/Hero/Hero.vue'
-import VideoBackgroundHero from './lib-components/blocks/Hero/VideoBackgroundHero.vue'
-import Modal from './lib-components/blocks/Modal/Modal.vue'
-import SubNavigation from './lib-components/blocks/Navigation/SubNavigation.vue'
-import AppTable from './lib-components/blocks/Table/AppTable.vue'
-import Testimonial from './lib-components/blocks/Testimonial/Testimonial.vue'
-import TextColumns from './lib-components/blocks/TextColumns/TextColumns.vue'
+const blocks = ref()
 
-// Elements
-import AppButton from './lib-components/elements/AppButton/AppButton.vue'
-
+onMounted(() => {
+  fetch('http://cms.test/api/public/1/posts?path=homepage')
+    .then((response) => response.json())
+    .then((data) => (blocks.value = data.data.blocks))
+    
+  // console.log(blocks)
+})
 
 const menu = {
   title: 'Main Navigation',
@@ -84,6 +81,52 @@ const menu = {
       href: '/get-started',
     },
   ]
+}
+</script>
+
+<script>
+// Globals
+import Navbar from './lib-components/globals/Header/Navbar.vue'
+
+// Blocks
+import Accordion from './lib-components/blocks/Accordion/Accordion.vue'
+import CardRepeater from './lib-components/blocks/CardRepeater/CardRepeater.vue'
+import DetailsList from './lib-components/blocks/DetailsList/DetailsList.vue'
+import Feature from './lib-components/blocks/Feature/Feature.vue'
+import FeatureV7 from './lib-components/blocks/Feature/FeatureV7.vue'
+import FeatureV9 from './lib-components/blocks/Feature/FeatureV9.vue'
+import FeatureV11 from './lib-components/blocks/Feature/FeatureV11.vue'
+import BoxedHero from './lib-components/blocks/Hero/BoxedHero.vue'
+import Hero from './lib-components/blocks/Hero/Hero.vue'
+import VideoBackgroundHero from './lib-components/blocks/Hero/VideoBackgroundHero.vue'
+import Modal from './lib-components/blocks/Modal/Modal.vue'
+import SubNavigation from './lib-components/blocks/Navigation/SubNavigation.vue'
+import AppTable from './lib-components/blocks/Table/AppTable.vue'
+import Testimonial from './lib-components/blocks/Testimonial/Testimonial.vue'
+import TextColumns from './lib-components/blocks/TextColumns/TextColumns.vue'
+
+// Elements
+import AppButton from './lib-components/elements/AppButton/AppButton.vue'
+
+export default {
+    components: {
+      Navbar,
+      Accordion,
+      CardRepeater,
+      DetailsList,
+      Feature,
+      FeatureV7,
+      FeatureV9,
+      FeatureV11,
+      BoxedHero,
+      Hero,
+      VideoBackgroundHero,
+      Modal,
+      SubNavigation,
+      AppTable,
+      Testimonial,
+      TextColumns,
+    }
 }
 </script>
 
--- src/lib-components/blocks/Table/AppTable.vue
+++ src/lib-components/blocks/Table/AppTable.vue
@@ -1,45 +1,59 @@
 <template>
   <section 
     :class="classBinds" 
-    class="container max-width-adaptive-lg"
+    class="app-table"
   >
-    <table class="table table--loaded table--expanded table--expanded@xs position-relative z-index-1 width-100% text-unit-em text-sm js-table" aria-label="Table Example">
-      <thead class="table__header">
-        <tr class="table__row">
-          <th
-            v-for="(column, index) in columns" 
+    <div class="container max-width-adaptive-lg">
+      <div class="content-wrapper position-relative max-width-sm margin-bottom-lg z-index-2">
+        <ContentComponent
+          :align="align"
+          :title="title"
+          :subtitle="subtitle"
+          :headingLevel="config.headingLevel"
+          :headingSize="config.headingSize"
+        />
+      </div>
+      <table class="table table--loaded table--expanded table--expanded@xs position-relative z-index-1 width-100% text-unit-em text-sm js-table" aria-label="Table Example">
+        <thead class="table__header">
+          <tr class="table__row">
+            <th
+              v-for="(column, index) in columns" 
+              :key="index"
+              class="table__cell text-left"
+              scope="col"
+            >
+              {{ column.content }}
+            </th>
+          </tr>
+        </thead>
+        <tbody class="table__body">
+          <tr 
+            v-for="(row, index) in rows"
             :key="index"
-            class="table__cell text-left"
-            scope="col"
+            class="table__row"
           >
-            {{ column.content }}
-          </th>
-        </tr>
-      </thead>
-      <tbody class="table__body">
-        <tr 
-          v-for="(row, index) in rows"
-          :key="index"
-          class="table__row"
-        >
-          <td 
-            v-for="(cell, index) in row"
-            :key="index"
-            class="table__cell" 
-            role="cell"
-          >
-            <span class="table__label" aria-hidden="true">{{ columns[index] }}:</span> 
-            {{ cell.content }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            <td 
+              v-for="(cell, index) in row"
+              :key="index"
+              class="table__cell" 
+              role="cell"
+            >
+              <span class="table__label" aria-hidden="true">{{ columns[index] }}:</span> 
+              {{ cell.content }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, PropType, computed } from '@vue/composition-api'
 import { appendToSet } from '@/helpers'
+
+// Components
+import ContentComponent from '@/lib-components/components/Content/ContentComponent.vue'
 
 // Types
 import { BlockConfig } from '@/types'
@@ -56,6 +70,21 @@ export default defineComponent({
         marginTop: 'none',
         marginBottom: 'none',
       })
+    },
+    
+    align: {
+      type: String as PropType<Align>,
+      default: 'left'
+    },
+    
+    title: {
+      type: String,
+      default: 'The title'
+    },
+    
+    subtitle: {
+      type: String,
+      default: 'The subtitle'
     },
     
     columns: {
@@ -110,6 +139,9 @@ export default defineComponent({
       if (props.config.marginBottom != 'none') {
         classSet = appendToSet(`margin-bottom-${props.config.marginBottom}`, classSet)
       }
+      if (props.align) {
+        classSet = appendToSet(`app-table--${props.align}`, classSet)
+      }
 
       return classSet
     })
@@ -117,153 +149,12 @@ export default defineComponent({
     return {
       classBinds
     }
-  }
+  },
+  
+  components: { ContentComponent }
 })
 </script>
 
 <style lang="scss">
-// @use '@/styles/base' as *;
-@import '@/styles/base/mixins';
-@import '@/styles/base/breakpoints';
-
-/* --------------------------------
-
-File#: _1_table
-Title: Table
-Descr: Data tables used to organize and display information in rows and columns
-Usage: codyhouse.co/license
-
--------------------------------- */
-
-// >>> style affecting all (block + expanded) versions 👇
-.table {
-  position: relative;
-  z-index: 1;
-}
-// <<< end style affecting all versions
-
-// >>> block version only (mobile) 👇
-.table:not(.table--expanded) {
-  border-collapse: separate;
-  border-spacing: 0 var(--space-md); // row gap
-  margin-top: calc(-2 * var(--space-md)); // set spacing variable = row gap ☝️
-
-  .table__header {
-    // hide table header - but keep it accessible to SR
-    @include srHide;
-  }
-
-  .table__row {
-    .table__cell:first-child {
-      border-radius: var(--radius-md) var(--radius-md) 0 0;
-    }
-
-    .table__cell:last-child {
-      border-radius: 0 0 var(--radius-md) var(--radius-md);
-
-      &::after {
-        // hide border bottom
-        display: none;
-      }
-    }
-  }
-
-  .table__cell {
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    text-align: right;
-    padding: var(--space-md);
-    background-color: var(--color-bg-light);
-
-    &::after {
-      // border bottom
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: var(--space-md);
-      width: calc(100% - (2 * var(--space-md)));
-      height: 1px;
-      background-color: var(--color-contrast-lower);
-    }
-  }
-
-  .table__label {
-    // inline labels -> visible when table header is hidden
-    font-weight: bold;
-    text-align: left;
-    color: var(--color-contrast-higher);
-    margin-right: var(--space-md);
-  }
-}
-// <<< end block version
-
-// >>> expanded version only (desktop) 👇 -> show standard rows and cols
-.table--expanded {
-  border-bottom: 1px solid var(--color-contrast-lower); // table border bottom
-
-  .table__header {
-    .table__cell {
-      // header cell style
-      position: relative;
-      z-index: 10;
-      background-color: var(--color-bg);
-      border-bottom: 1px solid var(--color-contrast-lower); // header border bottom
-      font-weight: bold;
-      color: var(--color-contrast-higher);
-    }
-  }
-
-  .table__body {
-    .table__row {
-      &:nth-child(odd) {
-        background-color: alpha(var(--color-bg-dark), 0.85);
-      }
-    }
-  }
-
-  .table__cell {
-    padding: var(--space-sm);
-  }
-
-  .table__label {
-    // hide inline labels
-    display: none;
-  }
-
-  // --header-sticky
-  .table__header--sticky {
-    .table__cell {
-      // header cell style
-      position: sticky;
-      top: 0;
-    }
-  }
-}
-// <<< end expanded version
-
-.js {
-  .table {
-    opacity: 0; // hide table while it is initialized in JS
-  }
-
-  .table--loaded {
-    opacity: 1;
-  }
-}
-
-// detect when the table needs to switch from the mobile layout to an expanded one - used in JS
-[class*="table--expanded"]::before {
-  display: none;
-}
-
-@each $breakpoint, $value in $breakpoints {
-  .table--expanded\@#{$breakpoint}::before {
-    content: "collapsed";
-    @include breakpoint(#{$breakpoint}) {
-      content: "expanded";
-    }
-  }
-}
+@import './style/style.scss';
 </style>
